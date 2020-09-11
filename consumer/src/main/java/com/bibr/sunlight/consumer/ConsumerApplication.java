eb041ae4d8690ccd6f1bdd815ed54c1358ed2a49
李林 <3086961507@qq.com>
2020-09-11 18:58:38 +0800

    增加 import

--- consumer/src/main/java/com/bibr/sunlight/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/bibr/sunlight/consumer/ConsumerApplication.java
@@ -3,6 +3,7 @@ package com.bibr.sunlight.consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
